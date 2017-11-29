88e77f93020c6833d8e0bad2d4d48da266d94742
Rob Fuentes <rob.fuentes@utexas.edu>
2017-11-29 17:17:34 -0600

    homework5

--- homework5/question2/plotData.m
+++ homework5/question2/plotData.m
@@ -1,32 +1,14 @@
 triggerListURL = 'http://www.shahmoradi.org/ICP2017F/homework/5-problems/triggers.txt';
 TriggerList = webread(triggerListURL);
 TriggerList = strsplit( TriggerList, '\n');
-dataDir = './swift/';
-mkdir(dataDir);
-missingFileCounter = 0;
-for i = 1:length(TriggerList)
-    dataRepos = 'http://www.shahmoradi.org/ICP2017F/homework/5-problems/swift/';
-    filename = ['GRB',TriggerList{i},'_ep_flu.txt'];
-    dataURL = [dataRepos,filename];
-try
-    disp(['Fetching file number: ', num2str(i)]);
-    data = webread(dataURL);
-    fid = fopen([dataDir,filename], 'w');
-    fprintf(fid,'%s',data);
-    fclose(fid);
-catch
-    missingFileCounter = missingFileCounter + 1;
-    disp('The requested file does not exist on the web! Skipping...');
-    disp(['Total number of missing files: ', num2str(missingFileCounter)]);
-end
-end
-dataCell = num2cell(data);
+figure(); hold on; box on;
+eventCounter = 0; 
 for i=1:length(TriggerList)
     disp(['Plotting data from file number: ', num2str(i)]);
-    if ~isempty(dataCell{i}) && all(dataCell{i}(:,2)<0)
+    if ~isempty(data{i}) && all(data{i}(:,2)<0)
         eventCounter = eventCounter + 1;
-        scatter( exp(dataCell{i}(:,2))...
-            , fid{i}(:,1) ...
+        scatter( exp(data{i}(:,2))...
+            , data{i}(:,1) ...
             , 1 ...
             , 'MarkerFaceColor', 'red' ...
             , 'MarkerEdgeColor', 'red' ...
